137087af9c02c0f218e7d74ed174eaa7011b9914
edorejel <110068017+edorejel@users.noreply.github.com>
2024-12-18 21:26:36 -0800

    Update QuickSort.java

--- sorting_algorithms/QuickSort/sample/QuickSort.java
+++ sorting_algorithms/QuickSort/sample/QuickSort.java
@@ -54,27 +54,4 @@ public class QuickSort {
         quicksort(numbers, lowEndIndex + 1, highIndex);
     }
 
-    public static void main(String[] args) {
-        // Example array
-        int[] numbers = {10, 2, 78, 4, 45, 32, 7, 11};
-        int NUMBERS_SIZE = numbers.length;
-
-        // Print unsorted array
-        System.out.print("UNSORTED: ");
-        for (int i = 0; i < NUMBERS_SIZE; i++) {
-            System.out.print(numbers[i] + " ");
-        }
-        System.out.println();
-
-        // Perform Quicksort
-        quicksort(numbers, 0, NUMBERS_SIZE - 1);
-
-        // Print sorted array
-        System.out.print("SORTED: ");
-        
-        for (int i = 0; i < NUMBERS_SIZE; i++) {
-            System.out.print(numbers[i] + " ");
-        }
-        System.out.println();
-    }
 }
